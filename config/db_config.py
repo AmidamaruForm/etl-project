57d872f22e37d13852cf18540a3352a9416dc7f4
aleksov <aleksov.dev@gmail.com>
2026-05-31 20:06:56 +0300

    Add changes to db_config file to you environment variables

--- config/db_config.py
+++ config/db_config.py
@@ -1,5 +1,10 @@
-DB_HOST = "localhost"
-DB_PORT = "5432"
-DB_NAME = "crypto_dwh"
-DB_USER = "postgres"
-DB_PASSWORD = "12345"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
